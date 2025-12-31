0755b75a32b38215f0c4ae88f17547a8197b8af7
Aviv Bichachi <96205729+AvivBichachi@users.noreply.github.com>
2025-12-31 10:32:15 +0200

    refactor: split app and server and add basic config (#6)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,5 @@
-const express = require("express");
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.get("/health", (req,res) => {
-    res.status(200).json({status: "ok"});
-});
-
-app.listen(PORT, () => {
-    console.log(`backend is running on http://localhost:${PORT}`);
+const app = require("./app");
+const config = require("./config")
+app.listen(config.port, () => {
+    console.log(`backend is running on http://localhost:${config.port}`);
 });
